2ad1fef70f0804cab96c06d3f2ff9901a223c2b7
Apip Kurniawan <davitaja92@gmail.com>
2023-09-21 22:27:43 +0700

    reusable form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { AddressComponent } from './components/address/address.component';
+import { FormGroup, ReactiveFormsModule, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   standalone: true,
-  imports: [RouterModule, AddressComponent],
+  imports: [RouterModule, ReactiveFormsModule, AddressComponent],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  form = new FormGroup({
+    displayName: new FormControl(''),
+  });
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  submit() {
+    console.log(this.form.value);
+    this.form.reset();
+  }
+}
--- src/app/components/address/address.component.ts
+++ src/app/components/address/address.component.ts
@@ -1,9 +1,42 @@
-import { Component } from '@angular/core';
-
+import { Component, inject, OnInit, OnDestroy, Input } from '@angular/core';
+import {
+  FormGroup,
+  ReactiveFormsModule,
+  FormControl,
+  ControlContainer,
+} from '@angular/forms';
 @Component({
   selector: 'app-address',
   templateUrl: './address.component.html',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
+  viewProviders: [
+    {
+      provide: ControlContainer,
+      useFactory: () => inject(ControlContainer, { skipSelf: true }),
+    },
+  ],
 })
-export class AddressComponent {}
+export class AddressComponent implements OnInit, OnDestroy {
+  @Input() controlKey = '';
+  @Input() label = '';
+  parentContainer = inject(ControlContainer);
+
+  get parentFormGroup() {
+    return this.parentContainer.control as FormGroup;
+  }
+
+  ngOnInit(): void {
+    this.parentFormGroup.addControl(
+      this.controlKey,
+      new FormGroup({
+        street: new FormControl(''),
+        zipCode: new FormControl(''),
+      })
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.parentFormGroup.removeControl(this.controlKey);
+  }
+}
