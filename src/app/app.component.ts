83d703cbf523cc519b4e3dbe8b6aec0c3f10d796
Apip Kurniawan <davitaja92@gmail.com>
2023-09-23 14:22:44 +0700

    delete another lesson

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { FormGroup, ReactiveFormsModule, FormControl } from '@angular/forms';
-import { AddressComponent } from './components/address/address.component';
-import { WidgetComponent } from './components/widget/widget.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   standalone: true,
-  imports: [
-    RouterModule,
-    ReactiveFormsModule,
-    AddressComponent,
-    WidgetComponent,
-  ],
+  imports: [RouterModule],
 })
 export class AppComponent implements OnInit {
-  form = new FormGroup({
-    displayName: new FormControl(''),
-  });
-
   constructor() {}
 
   ngOnInit(): void {}
-
-  submit() {
-    console.log(this.form.value);
-    this.form.reset();
-  }
 }
