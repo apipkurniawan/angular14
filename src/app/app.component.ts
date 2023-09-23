5be5265195df4861283f008f13e470999afed5b4
Apip Kurniawan <davitaja92@gmail.com>
2023-09-23 14:35:27 +0700

    single responsibility principle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { WidgetComponent } from './widget.component';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
   standalone: true,
-  imports: [RouterModule],
+  imports: [MatIconModule, MatToolbarModule, WidgetComponent],
+  template: `
+    <mat-toolbar color="primary">
+      <span>My App</span>
+    </mat-toolbar>
+    <main class="content">
+      <!-- single responsibility principle : memisahkan component menjadi component terpisah -->
+      <app-widget></app-widget>
+    </main>
+  `,
+  styles: [
+    `
+      .content {
+        background-color: #fff;
+        padding: 2rem;
+        height: calc(100vh - 64px);
+        display: flex;
+        box-sizing: border-box;
+        justify-content: center;
+        align-items: center;
+      }
+    `,
+  ],
 })
-export class AppComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
-}
+export class AppComponent {}
